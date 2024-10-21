99b2b23bcf147dd012240192f11dc4f5c6dff732
gauthierauge <yoyoyoyo.1234@hotmail.com>
2024-10-21 12:56:33 +0200

    Updating Archives logical

--- src/app/(main)/(chat)/archives/layout.tsx
+++ src/app/(main)/(chat)/archives/layout.tsx
@@ -1,106 +1,70 @@
-'use client';
+'use client'
 
-import React, {useEffect, useState} from 'react';
+import React, {useCallback, useEffect, useMemo, useState} from 'react';
 import {Loader2} from 'lucide-react';
 import ArchivesConversationItem from '@/app/(main)/(chat)/archives/components/ArchivesConversationItem';
 import ItemList from '@/app/(main)/(chat)/components/item/ItemList';
 import {fetchArchivedConversations, handleUnarchiveAllConversations} from '@/app/(main)/(chat)/archives/actions';
 import {useAuthContext} from '@/context/authContext';
-import {getLastMessageFromUser} from '@/app/(main)/(chat)/conversations/actions';
 import SearchBar from '@/app/(main)/(chat)/components/item/ItemSearchBar';
 import {Checkbox} from "@/components/ui/checkbox";
 import {CheckedState} from "@radix-ui/react-checkbox";
 import {toast} from "@/components/ui/use-toast";
-
-type Conversation = {
-    id: string;
-    isGroup: boolean;
-    lastMessage?: {
-        content: string;
-        sender: {
-            id: string;
-            userName: string;
-        };
-    };
-    participants: Array<{
-        id: string;
-        userName: string;
-        imageUrl?: string;
-    }>;
-    isArchived: boolean;
-    archivedAt: string;
-};
+import {ChatConversation, LastMessage} from "@/models/ChatConversation";
 
 const ArchivesLayout = ({ children }: { children: React.ReactNode }) => {
-    const [archivesConversation, setArchivesConversation] = useState<Conversation[]>([]);
-    const [filteredConversations, setFilteredConversations] = useState<Conversation[]>([]);
+    const [archivesConversation, setArchivesConversation] = useState<ChatConversation[]>([]);
+    const [filteredConversations, setFilteredConversations] = useState<ChatConversation[]>([]);
     const [selectedConversations, setSelectedConversations] = useState<Set<string>>(new Set());
     const [loading, setLoading] = useState<boolean>(true);
-    const [lastMessages, setLastMessages] = useState<{
-        [key: string]: {
-            senderEmail: string;
-            content: string;
-            senderName: string;
-            sentAt: string;
-            isRead: boolean;
-        };
-    }>({});
+
     const { user } = useAuthContext();
     const userId = user?.userId;
 
-    useEffect(() => {
-        const loadArchivedConversations = async () => {
-            try {
-                const data = await fetchArchivedConversations(userId);
-                setArchivesConversation(data);
-                setFilteredConversations(data);
+    const fetchArchivedConversationsData = useCallback(async () => {
+        if (!userId) return;
 
-                const updatedLastMessages: {
-                    [key: string]: {
-                        content: string;
-                        senderName: string;
-                        senderEmail: string;
-                        sentAt: string;
-                        isRead: boolean;
-                    };
-                } = {};
-
-                await Promise.all(
-                    data.map(async (conversation) => {
-                        const lastMessageData = await getLastMessageFromUser(conversation.id);
-                        if (lastMessageData && lastMessageData.data?.lastMessage) {
-                            updatedLastMessages[conversation.id] = {
-                                content: lastMessageData.data?.lastMessage.content,
-                                senderName: lastMessageData.data?.lastMessage.sent_by,
-                                senderEmail: lastMessageData.data?.lastMessage.sender_email,
-                                sentAt: lastMessageData.data?.lastMessage.send_at,
-                                isRead: lastMessageData.data?.lastMessage.isRead,
-                            };
-                        }
-                    })
-                );
-
-                setLastMessages(updatedLastMessages);
-            } catch (error) {
-                console.error('Erreur lors du chargement des conversations archivées:', error);
-                setArchivesConversation([]);
-            } finally {
-                setLoading(false);
-            }
-        };
-
-        if (userId) {
-            loadArchivedConversations();
+        setLoading(true);
+        try {
+            const data = await fetchArchivedConversations(userId);
+            setArchivesConversation(data);
+            setFilteredConversations(data);
+        } catch (error) {
+            setArchivesConversation([]);
+        } finally {
+            setLoading(false);
         }
     }, [userId]);
 
-    const getOtherMember = (conversation: Conversation) => {
-        return conversation.participants.find((participant) => participant.id !== userId);
+    useEffect(() => {
+        if (userId) {
+            fetchArchivedConversationsData();
+        }
+    }, [fetchArchivedConversationsData, userId]);
+
+    const getOtherMember = (conversation: ChatConversation) => {
+        if (Array.isArray(conversation.participants)) {
+            return conversation.participants.find(participant => participant.id !== userId);
+        }
+        return undefined;
     };
 
-    const getLastMessageSenderName = (conversationId: string): string => {
-        return lastMessages[conversationId]?.senderEmail === user.email ? "Vous " : lastMessages[conversationId]?.senderEmail || "Utilisateur inconnu";
-    };
+    const mutedConversationDetails = useMemo(() => {
+        return archivesConversation.map(conversation => {
+            const lastMessage = conversation.lastMessage as LastMessage|| {};
+            const otherMember = getOtherMember(conversation);
+            return {
+                id: conversation.id,
+                username: otherMember?.userName || "Utilisateur inconnu",
+                imageUrl: otherMember?.imageUrl || "",
+                lastMessageSender: lastMessage.sent_by || "Inconnu",
+                lastMessageContent: lastMessage.content || "",
+                archivedAt: conversation.archivedAt,
+                sentAt: lastMessage.sent_at,
+            };
+        });
+    }, [archivesConversation]);
+
     const toggleSelectAll = (isChecked: CheckedState) => {
         if (isChecked) {
             const allIds = new Set(filteredConversations.map(conversation => conversation.id));
@@ -119,45 +83,43 @@ const ArchivesLayout = ({ children }: { children: React.ReactNode }) => {
         }
         setSelectedConversations(updatedSelection);
     };
+
     const handleUnarchiveSelected = async (userId: string) => {
         try {
             await handleUnarchiveAllConversations(userId);
-
-            toast({
-                variant: "default",
-                description: "Toutes les conversations ont bien été archivées !"
-            });
+            showToast('success', 'Toutes les conversations ont bien été désarchivées !');
         } catch (error) {
-            const errorMessage = (error as Error).message || "Il y a eu un problème avec votre demande.";
-
-            toast({
-                variant: "destructive",
-                title: "Erreur",
-                description: errorMessage
-            })
+            const errorMessage = (error as Error).message || 'Il y a eu un problème avec votre demande.';
+            showToast('error', errorMessage);
         }
     };
 
-    const handleSearch = (searchTerm: string) => {
-        if (searchTerm) {
-            const filtered = archivesConversation.filter(conversation => {
-                const otherMember = getOtherMember(conversation);
-                const lastMessageContent = lastMessages[conversation.id]?.content || "";
-                return (
-                    otherMember?.userName.toLowerCase().includes(searchTerm.toLowerCase()) ||
-                    lastMessageContent.toLowerCase().includes(searchTerm.toLowerCase())
-                );
-            });
-            setFilteredConversations(filtered);
-        } else {
-            setFilteredConversations(archivesConversation);
-        }
+    const resetSearchBarConversations = () => {
+        setFilteredConversations(archivesConversation);
     };
+
+    const showToast = (variant: "default" | "destructive", description: string, title?: string) => {
+        toast({
+            variant,
+            title,
+            description,
+        });
+    };
+
 
     return (
         <div className="mt-16">
             <ItemList title="Archives">
-                <SearchBar placeholder="Rechercher une conversation..." onSearch={handleSearch}/>
+                <SearchBar
+                    placeholder="Rechercher une conversation..."
+                    items={archivesConversation}
+                    setFilteredItems={setFilteredConversations}
+                    getLabel={(conversation) => {
+                        const otherMember = getOtherMember(conversation);
+                        return otherMember?.userName || '';
+                    }}
+                    resetItems={resetSearchBarConversations}
+                />
                 <div className="grid grid-cols-3 items-center mt-2 text-xs">
                     <div className="flex mr-5 items-center">
                         <Checkbox
@@ -186,22 +148,18 @@ const ArchivesLayout = ({ children }: { children: React.ReactNode }) => {
                         Pas de conversation trouvée
                     </p>
                 ) : (
-                    filteredConversations.map((conversation) => {
-                        const otherMember = getOtherMember(conversation);
-                        const lastMessageContent = lastMessages[conversation.id]?.content || "";
-                        const archivedAt = conversation.archivedAt;
-
+                    mutedConversationDetails.map(({id, username, imageUrl, lastMessageSender, lastMessageContent, archivedAt}) => {
                         return (
                             <ArchivesConversationItem
-                                key={conversation.id}
-                                id={conversation.id}
-                                username={otherMember?.userName || 'Utilisateur inconnu'}
-                                imageUrl={otherMember?.imageUrl || ''}
+                                key={id}
+                                id={id}
+                                username={username}
+                                imageUrl={imageUrl}
                                 lastMessageContent={lastMessageContent}
-                                lastMessageSender={getLastMessageSenderName(conversation.id)}
-                                archivedAt={archivedAt}
-                                isChecked={selectedConversations.has(conversation.id)}
-                                onChange={() => toggleSelectConversation(conversation.id)}
+                                lastMessageSender={lastMessageSender}
+                                archivedAt={archivedAt || "Date inconnue"}
+                                isChecked={selectedConversations.has(id)}
+                                onChange={() => toggleSelectConversation(id)}
                             />
                         );
                     })
