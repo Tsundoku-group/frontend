921bedbe854175ba142c3762d15e1e1d573c2aa4
gauthierauge <yoyoyoyo.1234@hotmail.com>
2024-11-06 15:11:15 +0100

    profile model

---
+++ src/models/Profile.tsx
@@ -0,0 +1,19 @@
+export interface Profile {
+    id: number;
+    role?: string;
+    firstName: string;
+    lastName: string;
+    username: string;
+    avatarUrl?: string;
+    coverUrl?: string;
+    friendsCount?: number;
+    followersCount?: number;
+    birthday?: string | null;
+    gender?: string;
+    phoneNumber?: string;
+    bio: string;
+    x?: string;
+    instagram?: string;
+    facebook?: string;
+    createdAt?: string | null;
+}
