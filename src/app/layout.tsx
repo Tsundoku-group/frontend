46a7d9489d20a8d179571fc3c9abffc19ee36cc4
Jessica <ljessicalejeune@gmail.com>
2024-12-11 15:55:02 +0100

    create global css and import it into src app layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type {Metadata} from "next";
 import "./globals.css";
 import {AuthProvider} from "@/context/authContext";
 import {TooltipProvider} from "@/components/ui/tooltip";
+import "../global.css";
 
 export const metadata: Metadata = {
     title: "Tsundoku",
@@ -14,7 +15,7 @@ export const metadata: Metadata = {
 export default function RootLayout({children}: Readonly<{ children: React.ReactNode; }>) {
     return (
         <html lang="en">
-        <body className="bg-primary-black">
+        <body>
         <AuthProvider>
                 <TooltipProvider>
                     {children}
