b54dbbf5018a322f16eea5bdea0ab410861c1acd
gauthierauge <gauthier.auge@2dcom.fr>
2025-01-08 10:04:00 +0100

    Adding onClose() function to close dropdownMenu into navbar.tsx

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -285,7 +285,7 @@ export default function Navbar() {
                     style={{width: "100%"}}
                 >
                     <div className="w-full p-2 flex flex-col justify-center space-y-2 flex-shrink-0">
-                        <ProfileButton userId={userId} profileId={profileId} email={user?.email}/>
+                        <ProfileButton userId={userId} profileId={profileId} email={user?.email} onClose={() => setIsDropdownOpen(false)} />
                         <Button
                             className="flex justify-between w-full text-white bg-transparent outline-none focus:outline-none hover:bg-hover-bg-color hover:bg-gray-700 hover:text-gray-200 transition-colors duration-200 rounded-lg"
                             onClick={() => handleSwitch("", "profiles")}
@@ -325,8 +325,8 @@ export default function Navbar() {
                             </div>
                             <ChevronRight className="w-4"/>
                         </Button>
-                        <SettingsButton/>
-                        <LogoutButton/>
+                        <SettingsButton onClose={() => setIsDropdownOpen(false)}/>
+                        <LogoutButton onClose={() => setIsDropdownOpen(false)}/>
                     </div>
                     <div className="w-full p-1 flex-shrink-0">
                         {loading ? (
@@ -351,7 +351,7 @@ export default function Navbar() {
                                 >
                                     {items}
                                 </RadioGroup>
-                                {visibleItems === "profiles" && userProfiles.length < 5 && <AddProfileButton onProfileAdded={handleProfileAdded}/>}
+                                {visibleItems === "profiles" && userProfiles.length < 5 && <AddProfileButton onProfileAdded={handleProfileAdded}  onClose={() => setIsDropdownOpen(false)} />}
                             </>
                         )}
                         <Button
