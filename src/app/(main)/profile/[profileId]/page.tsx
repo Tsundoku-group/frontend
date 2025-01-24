f9396e1367a77fa5bf652128af4b757d380b2505
gauthierauge <gauthier.auge@2dcom.fr>
2025-01-24 10:50:47 +0100

    Adding icons into profileHeader

--- src/app/(main)/profile/[profileId]/page.tsx
+++ src/app/(main)/profile/[profileId]/page.tsx
@@ -41,7 +41,6 @@ const ProfilePage = React.memo(({params: {profileId}}: Props) => {
                     coverImageUrl: images.cover || "",
                 })
             }).catch((error) => {
-                console.error("Erreur lors de la récupération des images Firebase :", error);
                 setImagesOtherProfiles({ profileImageUrl: "", coverImageUrl: "" });
             });
         }
@@ -91,6 +90,7 @@ const ProfilePage = React.memo(({params: {profileId}}: Props) => {
                     facebook={profile?.facebook}
                     profileImageUrl={profileImageUrl}
                     coverImageUrl={coverImageUrl}
+                    lastTwoFriends={profile?.lastTwoFriends || []}
                     setActiveTab={setActiveTab}
                     isOwnProfile={isOwnProfile}
                 />
