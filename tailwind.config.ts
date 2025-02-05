68bc677e59910b98a5a440b7e29cba45bc26a1d1
gauthierauge <gauthier.auge@2dcom.fr>
2025-02-05 17:06:15 +0100

    Updating config tailwind

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,121 +10,66 @@ const config = {
     ],
     prefix: "",
     theme: {
-    	container: {
-    		center: true,
-    		padding: '2rem',
-    		screens: {
-    			sm: '480px',
-    			md: '768px',
-    			lg: '976px',
-    			xl: '1440px'
-    		}
-    	},
-    	extend: {
-    		colors: {
-    			'primary-black': '#13181F',
-    			'secondary-black': '#171C26',
-    			'tertiary-black': '#1D2330',
-    			'text-white': '#D2D5E9',
-    			'green-highlight': '#20D998',
-    			'red-highlight': '#FF5C64',
-    			'purple-highlight': '#8146FF',
-    			'yellow-highlight': '#FFBC33',
-    			primary: {
-    				DEFAULT: 'hsl(var(--primary))',
-    				foreground: 'hsl(var(--primary-foreground))'
-    			},
-    			secondary: {
-    				DEFAULT: 'hsl(var(--secondary))',
-    				foreground: 'hsl(var(--secondary-foreground))'
-    			},
-    			background: 'hsl(var(--background))',
-    			foreground: 'hsl(var(--foreground))',
-    			card: {
-    				DEFAULT: 'hsl(var(--card))',
-    				foreground: 'hsl(var(--card-foreground))'
-    			},
-    			popover: {
-    				DEFAULT: 'hsl(var(--popover))',
-    				foreground: 'hsl(var(--popover-foreground))'
-    			},
-    			muted: {
-    				DEFAULT: 'hsl(var(--muted))',
-    				foreground: 'hsl(var(--muted-foreground))'
-    			},
-    			accent: {
-    				DEFAULT: 'hsl(var(--accent))',
-    				foreground: 'hsl(var(--accent-foreground))'
-    			},
-    			destructive: {
-    				DEFAULT: 'hsl(var(--destructive))',
-    				foreground: 'hsl(var(--destructive-foreground))'
-    			},
-    			border: 'hsl(var(--border))',
-    			input: 'hsl(var(--input))',
-    			ring: 'hsl(var(--ring))',
-    			chart: {
-    				'1': 'hsl(var(--chart-1))',
-    				'2': 'hsl(var(--chart-2))',
-    				'3': 'hsl(var(--chart-3))',
-    				'4': 'hsl(var(--chart-4))',
-    				'5': 'hsl(var(--chart-5))'
-    			}
-    		},
-    		fontFamily: {
-    			roboto: [
-    				'Roboto',
-    				'sans-serif'
-    			],
-    			'open-sans': [
-    				'Open Sans',
-    				'sans-serif'
-    			],
-    			poppins: [
-    				'Poppins',
-    				'sans-serif'
-    			]
-    		},
-    		fontSize: {
-    			base: 'var(--text-size)'
-    		},
-    		keyframes: {
-    			'accordion-down': {
-    				from: {
-    					height: '0'
-    				},
-    				to: {
-    					height: 'var(--radix-accordion-content-height)'
-    				}
-    			},
-    			'accordion-up': {
-    				from: {
-    					height: 'var(--radix-accordion-content-height)'
-    				},
-    				to: {
-    					height: '0'
-    				}
-    			},
-    			bounce: {
-    				'0%, 100%': {
-    					transform: 'translateY(0)'
-    				},
-    				'50%': {
-    					transform: 'translateY(-20px)'
-    				}
-    			}
-    		},
-    		animation: {
-    			'accordion-down': 'accordion-down 0.2s ease-out',
-    			'accordion-up': 'accordion-up 0.2s ease-out',
-    			bounce: 'bounce 1.5s infinite'
-    		},
-    		borderRadius: {
-    			lg: 'var(--radius)',
-    			md: 'calc(var(--radius) - 2px)',
-    			sm: 'calc(var(--radius) - 4px)'
-    		}
-    	}
+		container: {
+			center: true,
+			padding: "2rem",
+			screens: {
+				sm: '480px',
+				md: '768px',
+				lg: '976px',
+				xl: '1440px',
+			},
+		},
+		extend: {
+			colors: {
+				'primary-black': '#13181F',
+				'secondary-black': '#171C26',
+				'tertiary-black': '#1D2330',
+
+				'text-white': '#D2D5E9',
+
+				'green-highlight': '#20D998',
+				'red-highlight': '#FF5C64',
+				'purple-highlight': '#8146FF',
+				'yellow-highlight': '#FFBC33',
+
+				primary: {
+					DEFAULT: '#353a45',
+					foreground: '#ffffff',
+				},
+				secondary: {
+					DEFAULT: '#2c3648',
+					foreground: '#d2d5e9',
+				},
+			},
+			fontFamily: {
+				roboto: ['Roboto', 'sans-serif'],
+				'open-sans': ['Open Sans', 'sans-serif'],
+				poppins: ['Poppins', 'sans-serif'],
+			},
+			fontSize: {
+				base: "var(--text-size)",
+			},
+			keyframes: {
+				"accordion-down": {
+					from: {height: "0"},
+					to: {height: "var(--radix-accordion-content-height)"},
+				},
+				"accordion-up": {
+					from: {height: "var(--radix-accordion-content-height)"},
+					to: {height: "0"},
+				},
+				bounce: {
+					"0%, 100%": {transform: "translateY(0)"},
+					"50%": {transform: "translateY(-20px)"},
+				},
+			},
+			animation: {
+				"accordion-down": "accordion-down 0.2s ease-out",
+				"accordion-up": "accordion-up 0.2s ease-out",
+				bounce: "bounce 1.5s infinite",
+			},
+		},
     },
     plugins: [require("tailwindcss-animate")],
 } satisfies Config
