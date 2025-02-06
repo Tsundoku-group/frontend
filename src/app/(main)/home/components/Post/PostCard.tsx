ce18d32e3e499937d7c682f04c656f8979dd13bf
gauthierauge <gauthier.auge@2dcom.fr>
2025-02-06 11:18:49 +0100

    Css: updating

--- src/app/(main)/home/components/Post/PostCard.tsx
+++ src/app/(main)/home/components/Post/PostCard.tsx
@@ -1,7 +1,8 @@
-import {Heart, Send} from "lucide-react";
+import {Heart, Send, User} from "lucide-react";
 import PostDate from "@/app/(main)/home/components/Post/PostDate";
 import CommentSection from "@/app/(main)/home/components/Comment/CommentSection";
 import {useState} from "react";
+import {Avatar, AvatarFallback, AvatarImage} from "@/components/ui/avatar";
 
 interface Post {
     id: string;
@@ -22,12 +23,11 @@ export default function PostCard({post}: { post: Post }) {
 
     return (
         <div className="bg-tertiary-black p-4 rounded-lg shadow-md w-full mb-6">
-            <div className="flex items-center gap-4">
-                <img
-                    src=""
-                    alt=""
-                    className="w-12 h-12 rounded-full object-cover"
-                />
+            <div className="flex items-center gap-4 ml-4">
+                <Avatar className="w-16 h-16">
+                    <AvatarImage src={post.author.avatar} />
+                    <AvatarFallback><User /></AvatarFallback>
+                </Avatar>
                 <div>
                     <div className="text-white font-semibold">{post.author.name} <span
                         className="text-gray-400">@{post.author.username}</span></div>
@@ -35,7 +35,7 @@ export default function PostCard({post}: { post: Post }) {
                 </div>
             </div>
 
-            <div className="text-white mt-3 mb-8 ml-8 text-sm">{post.content}</div>
+            <div className="text-white mt-3 px-12 pb-6 text-sm">{post.content}</div>
 
             {Array.isArray(post.images) && post.images.length > 0 && (
                 <div className="grid gap-2 mt-3 rounded-lg overflow-hidden"
@@ -53,7 +53,7 @@ export default function PostCard({post}: { post: Post }) {
                 </div>
             )}
 
-            <div className="flex justify-between items-center mt-3 px-16">
+            <div className="flex justify-between items-center mt-3 px-12 text-xs">
                 <div className="flex items-center gap-1 text-gray-400">
                     <Heart className="w-4 h-4 text-red-400"/>
                     <span className="font-semibold">4</span>
