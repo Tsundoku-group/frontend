ab74f00811db21048aa7c62d0f82ee7f97ffbd97
gauthierauge <gauthier.auge@2dcom.fr>
2025-02-06 13:42:15 +0100

    Fix: deleting index

--- src/app/(main)/home/components/RepliesSection.tsx
+++ src/app/(main)/home/components/RepliesSection.tsx
@@ -24,7 +24,7 @@ export default function RepliesSection({commentId}: RepliesSectionProps) {
                 <p className="text-gray-400 text-xs">Chargement des réponses...</p>
             ) : replies.length > 0 ? (
                 <div className="space-y-4">
-                    {replies.map((reply: any, index) => (
+                    {replies.map((reply: any) => (
                         <div key={reply.id} className="relative flex items-start text-xs pl-6">
                             <span
                                 className="absolute left-2.5 bottom-4 w-2 border-t-2 border-gray-700 before:content-[''] before:absolute before:w-3 before:h-3 before:-left-3 before:bottom-0 before:border-l-2 before:border-b-2 before:rounded-bl-md before:border-gray-700"></span>
