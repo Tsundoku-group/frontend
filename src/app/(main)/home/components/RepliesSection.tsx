f34dc973ab4e140633e78a02c92ef33e1760ebba
gauthierauge <gauthier.auge@2dcom.fr>
2025-02-06 13:41:53 +0100

    Adding horizontal line

--- src/app/(main)/home/components/RepliesSection.tsx
+++ src/app/(main)/home/components/RepliesSection.tsx
@@ -2,8 +2,8 @@
 
 import { useQuery } from "@tanstack/react-query";
 import { fetchRepliesForComment } from "@/app/(main)/home/actions";
-import {Avatar, AvatarFallback, AvatarImage} from "@/components/ui/avatar";
-import {User} from "lucide-react";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { User } from "lucide-react";
 
 interface RepliesSectionProps {
     commentId: string;
@@ -19,18 +19,24 @@ export default function RepliesSection({ commentId }: RepliesSectionProps) {
     const replies = Array.isArray(data?.replies) ? data.replies : [];
 
     return (
-        <div className="ml-10 mt-2 border-l-2 border-gray-700 pl-4">
+        <div className="mt-2 border-l-2 border-gray-700">
             {isLoading ? (
                 <p className="text-gray-400 text-xs">Chargement des réponses...</p>
             ) : replies.length > 0 ? (
-                <div className="space-y-2">
-                    {replies.map((reply: any) => (
-                        <div key={reply.id} className="flex gap-3 items-start text-xs">
-                            <Avatar className="w-8 h-8 rounded-full">
-                                <AvatarImage></AvatarImage>
-                                <AvatarFallback><User /></AvatarFallback>
-                            </Avatar>
-                            <div className="bg-secondary-black p-2 rounded-lg">
+                <div className="space-y-4">
+                    {replies.map((reply: any, index) => (
+                        <div key={reply.id} className="relative flex items-start text-xs pl-6">
+                            <span className="absolute left-2.5 bottom-4 w-2 border-t-2 border-gray-700 before:content-[''] before:absolute before:w-3 before:h-3 before:-left-3 before:bottom-0 before:border-l-2 before:border-b-2 before:rounded-bl-md before:border-gray-700"></span>
+                            <div className="relative">
+                                <Avatar className="w-8 h-8 rounded-full bg-secondary-black">
+                                    <AvatarImage />
+                                    <AvatarFallback>
+                                        <User />
+                                    </AvatarFallback>
+                                </Avatar>
+                            </div>
+
+                            <div className="bg-secondary-black p-2 rounded-lg ml-2 w-full">
                                 <p className="text-white font-semibold"></p>
                                 <p className="text-gray-300">{reply.content}</p>
                             </div>
