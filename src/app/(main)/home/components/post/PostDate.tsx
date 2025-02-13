87a8a42ce569a1bf7c9939568cf64ff4e52d7b6a
gauthierauge <gauthier.auge@2dcom.fr>
2025-02-13 11:40:21 +0100

    updating PostDate.tsx for more validation

--- src/app/(main)/home/components/post/PostDate.tsx
+++ src/app/(main)/home/components/post/PostDate.tsx
@@ -3,10 +3,15 @@ import { fr } from "date-fns/locale";
 import { useMemo } from "react";
 
 export default function PostDate({ date }: { date: string }) {
-    const parsedDate = useMemo(() => new Date(date), [date]);
+    const parsedDate = useMemo(() => {
+        if (!date || isNaN(Date.parse(date))) {
+            return null;
+        }
+        return new Date(date);
+    }, [date]);
 
     const timeAgo = useMemo(() =>
-            parsedDate ? formatDistanceToNowStrict(parsedDate, { addSuffix: true, locale: fr }) : "",
+            parsedDate ? formatDistanceToNowStrict(parsedDate, { addSuffix: true, locale: fr }) : "Date invalide",
         [parsedDate]);
 
     return <div className="text-green-400 text-xs">{timeAgo}</div>;
