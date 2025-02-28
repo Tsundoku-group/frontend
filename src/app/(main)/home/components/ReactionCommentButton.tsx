b279bf7d04dffd496244f9e2c664c89b141f9346
gauthierauge <gauthier.auge@2dcom.fr>
2025-02-28 15:41:15 +0100

    add ReactionCommentButton.tsx

---
+++ src/app/(main)/home/components/ReactionCommentButton.tsx
@@ -0,0 +1,56 @@
+import {Heart} from "lucide-react";
+import {useState, useEffect} from "react";
+import {likePost} from "@/app/(main)/home/actions";
+import {ShowToast} from "@/components/ShowToast";
+
+interface ReactionCommentButtonProps {
+    commentId: string;
+    profileId?: string;
+    actorFirstName?: string;
+    receiverId: string;
+    resourceType: "POST" | "COMMENT";
+    initialHasLiked: boolean;
+}
+
+export default function ReactionCommentButton({
+                                                  commentId,
+                                                  profileId,
+                                                  receiverId,
+                                                  resourceType,
+                                                  initialHasLiked
+                                              }: ReactionCommentButtonProps) {
+    const [hasLiked, setHasLiked] = useState(initialHasLiked);
+
+    useEffect(() => {
+        setHasLiked(initialHasLiked);
+    }, [initialHasLiked]);
+
+    const handleLikeCommentToggle = async () => {
+        if (!profileId) {
+            return;
+        }
+
+        const newHasLiked = !hasLiked;
+        setHasLiked(newHasLiked);
+        console.log('commentId :', commentId)
+        try {
+            await likePost(profileId, receiverId, resourceType, commentId, "LIKE");
+        } catch (err) {
+            setHasLiked(!newHasLiked);
+            ShowToast("destructive", "Une erreur est survenue.", "Erreur");
+        }
+    };
+
+    return (
+        <button
+            className="flex items-center gap-1 text-gray-400 hover:text-gray-500"
+            onClick={() => handleLikeCommentToggle()}
+        >
+            <Heart
+                className="w-4 h-4 "
+                fill={hasLiked ? "currentColor" : "none"}
+                stroke="currentColor"
+            /> J’aime
+        </button>
+    );
+}
