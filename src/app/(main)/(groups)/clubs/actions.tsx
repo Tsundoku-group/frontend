b0693d2c3dc67cfae7da0ac29f9f688874d7234a
gauthierauge <gauthier.auge@2dcom.fr>
2025-03-10 15:10:04 +0100

    add actions for search privates groups

---
+++ src/app/(main)/(groups)/clubs/actions.tsx
@@ -0,0 +1,35 @@
+'use server';
+
+import { fetchWithAuth } from "@/services/fetchWithAuth";
+import { GroupData } from "@/models/GroupData";
+
+const symfonyUrl = process.env.SYMFONY_URL;
+
+export const fetchPrivateGroups = async (
+    search: string,
+    tagName: string,
+    sort: string,
+    page: number,
+    limit: number = 20
+): Promise<{ groups: GroupData[], nextPage: number | null }> => {
+    try {
+        const queryTag = tagName === "all" ? "" : tagName;
+        const response = await fetchWithAuth(
+            `${symfonyUrl}/api/v1/group/private?search=${encodeURIComponent(search)}&tagName=${encodeURIComponent(queryTag)}&sort=${encodeURIComponent(sort)}&page=${page}&limit=${limit}`,
+            {
+                method: "GET",
+                headers: {
+                    "Content-Type": "application/json",
+                }
+            }
+        );
+        if (!response.response || response.status !== 200) {
+            throw new Error("Not Found");
+        }
+
+        return response.data;
+    } catch (error) {
+        console.error(error);
+        return { groups: [], nextPage: null };
+    }
+};
