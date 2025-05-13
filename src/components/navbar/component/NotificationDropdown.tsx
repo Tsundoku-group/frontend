414ac7a4f3c64d17eeeac30a88c870f4984c6418
gauthierauge <gauthier.auge@2dcom.fr>
2025-05-13 12:15:38 +0200

    upgrade w & h for alert red point

--- src/components/navbar/component/NotificationDropdown.tsx
+++ src/components/navbar/component/NotificationDropdown.tsx
@@ -96,7 +96,7 @@ export default function NotificationDropdown() {
                 <div className="relative cursor-pointer">
                     <Bell className="text-text-white w-6 h-6 fill-current hover:animate-shake transition-transform origin-top" />
                     {hasUnread && (
-                        <div className="absolute top-0 right-0 w-2 h-2 bg-red-highlight rounded-full"/>
+                        <div className="absolute top-0 right-0 w-3 h-3 bg-red-highlight rounded-full"/>
                     )}
                 </div>
             </DropdownMenuTrigger>
