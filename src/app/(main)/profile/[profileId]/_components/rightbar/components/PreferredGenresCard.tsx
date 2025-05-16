e4fdb5bbb0d1ffc6dcc0c1348b5e66dd11cb9253
gauthierauge <gauthier.auge@2dcom.fr>
2025-05-16 11:09:07 +0200

    fix: delete unused imports

--- src/app/(main)/profile/[profileId]/_components/rightbar/components/PreferredGenresCard.tsx
+++ src/app/(main)/profile/[profileId]/_components/rightbar/components/PreferredGenresCard.tsx
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { Card } from "@/components/ui/card";
 import { Badge } from "@/components/ui/badge";
-import { Library } from "lucide-react";
 import StatsIcon from "@/assets/icons/StatsIcon";
 
 const genresData = [
