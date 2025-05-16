b89b74df32f39b8687a8cdcb30fcd07d064d503c
gauthierauge <gauthier.auge@2dcom.fr>
2025-05-16 11:08:42 +0200

    handle css into genres

--- src/app/(main)/profile/[profileId]/_components/rightbar/components/PreferredGenresCard.tsx
+++ src/app/(main)/profile/[profileId]/_components/rightbar/components/PreferredGenresCard.tsx
@@ -2,13 +2,13 @@ import React, { useEffect, useState } from "react";
 import { Card } from "@/components/ui/card";
 import { Badge } from "@/components/ui/badge";
 import { Library } from "lucide-react";
+import StatsIcon from "@/assets/icons/StatsIcon";
 
 const genresData = [
     { name: "Fantaisie", color: "bg-yellow-500", percentage: 66 },
-    { name: "Science-fiction", color: "bg-purple-500", percentage: 25 },
+    { name: "Science-fiction", color: "bg-orange-500", percentage: 25 },
     { name: "Fantastique", color: "bg-red-400", percentage: 40 },
     { name: "Solar Punk", color: "bg-green-500", percentage: 10 },
-    { name: "Historique", color: "bg-amber-100", percentage: 20 },
 ];
 
 const PreferredGenresCard = () => {
@@ -34,16 +34,18 @@ const PreferredGenresCard = () => {
     }, [genres]);
 
     return (
-        <Card className="bg-secondary-black p-4 border-spacing-1 border-gray-600">
-            <h2 className="text-text-white text-lg font-semibold flex items-center">
-                <Library className="w-6 h-6 mr-2" /> Genres préférés
-            </h2>
-            <div className="space-y-4 mt-3">
+        <Card className="bg-secondary-black p-8 border-spacing-1 border-tertiary-black">
+            <div className="text-text-white text-lg font-semibold flex items-center">
+                <StatsIcon className="w-6 h-6 mr-2 fill-current text-text-white" /> Genres préférés
+            </div>
+            <div className="space-y-3 mt-3 p-2">
                 {genres.map((genre, index) => (
                     <div key={index} className="flex flex-col space-y-1">
                         <div className="flex items-center justify-between">
-                            <Badge className="bg-dark-700 text-white max-w-max px-2">
-                                {genre.name}
+                            <Badge
+                                className={`bg-primary-black ${genre.color.replace("bg-", "text-")} max-w-max px-2`}
+                            >
+                                {genre.name.toLowerCase()}
                             </Badge>
                             <span className="text-text-white text-sm">
                                 {genre.currentPercentage}%
@@ -51,7 +53,7 @@ const PreferredGenresCard = () => {
                         </div>
                         <div className="w-full h-2 bg-gray-800 rounded-full">
                             <div
-                                className={`h-full ${genre.color} rounded-full`}
+                                className={`h-full ${genre.color} rounded-full transition-all duration-300 ease-in-out`}
                                 style={{ width: `${genre.currentPercentage}%` }}
                             />
                         </div>
