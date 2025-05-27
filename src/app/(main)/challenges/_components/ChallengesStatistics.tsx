9d1c2cbd870ceb38c13ac67c0b8aad39e5ec4d76
Jessica <ljessicalejeune@gmail.com>
2025-05-27 23:01:09 +0200

    handle no data provided ChallengesStatistics

--- src/app/(main)/challenges/_components/ChallengesStatistics.tsx
+++ src/app/(main)/challenges/_components/ChallengesStatistics.tsx
@@ -19,18 +19,18 @@ export default function ChallengesStatistics({
       <div className="grid grid-cols-2 gap-4">
         <div className="flex flex-col">
           <span className="mb-2">
-            <span className="text-yellow-highlight font-bold text-3xl">{acceptedCount}</span> défis acceptés
+            <span className="text-yellow-highlight font-bold text-3xl">{acceptedCount ? acceptedCount : "0"}</span> défis acceptés
           </span>
           <span>
-            <span className="text-red-highlight font-bold text-3xl">{successRate}%</span> défis réussis
+            <span className="text-red-highlight font-bold text-3xl">{successRate ? successRate : "0"}%</span> défis réussis
           </span>
         </div>
         <div className="flex flex-col">
           <span className="mb-2">
-            <span className="text-purple-highlight font-bold text-3xl">{createdCount}</span> défis créés
+            <span className="text-purple-highlight font-bold text-3xl">{createdCount ? createdCount : "0"}</span> défis créés
           </span>
           <span>
-            <span className="text-green-highlight font-bold text-3xl">{badgesCount}</span> badges obtenus
+            <span className="text-green-highlight font-bold text-3xl">{badgesCount ? badgesCount : "0"}</span> badges obtenus
           </span>
         </div>
       </div>
